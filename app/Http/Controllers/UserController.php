6ae62e445ab733cabf24ca6acca4b6480f80ee8d
andika1991 <andikapsw30@gmail.com>
2024-09-24 19:32:47 +0700

    up

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Kelas; // Bagian ini diambil dari branch database-model
+use App\Models\Kelas; 
 use App\Models\UserModel;
 
 class UserController extends Controller
